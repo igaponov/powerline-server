7ed1424c0ee546acfcb18145ee35b32661c15eb9
Ángel Guzmán Maeso <shakaran@gmail.com>
2016-02-29 03:09:53 +0100

    <feature> Add docs for answersListAction in Leader Poll controller

--- backend/src/Civix/ApiBundle/Controller/Leader/PollController.php
+++ backend/src/Civix/ApiBundle/Controller/Leader/PollController.php
@@ -43,14 +43,33 @@ class PollController extends BaseController
     }
 
     /**
+     * List all the answers for a given question.
+     * 
+     * The current user only can list the answers if it is the question user creator.
+     * 
      * @Route("/{id}/answers/")
      * @Method("GET")
+     * 
+     * @ApiDoc(
+     *     resource=true,
+     *     description="List all the answers for a given question.",
+     *     statusCodes={
+     *         200="Returns list",
+     *         400="Bad Request",
+     *         401="Authorization required",
+     *         404="Question not found",
+     *         405="Method Not Allowed"
+     *     }
+     * )
      */
     public function answersListAction(Question $question)
     {
+    	// Forbid list the answers if the current user is different for the question user
         if ($question->getUser() !== $this->getUser()) {
             return $this->createJSONResponse('', 403);
         }
+        
+        // Fetch all the answers for the question selected
         $answers = $this->getDoctrine()->getRepository(Answer::class)->findByQuestion($question);
 
         return $this->createJSONResponse($this->jmsSerialization($answers, ['api-leader-answers']));
