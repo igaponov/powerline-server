47c3317eba9ea0b4a4d74ffc35c7f005c0e385a2
Igor Gaponov <jiminy96@gmail.com>
2016-03-03 12:07:07 +0700

    Add petition summary description

---
+++ backend/src/Civix/CoreBundle/Entity/Micropetitions/Metadata.php
@@ -0,0 +1,85 @@
+<?php
+namespace Civix\CoreBundle\Entity\Micropetitions;
+
+use JMS\Serializer\Annotation as Serializer;
+
+class Metadata
+{
+    /**
+     * @var string
+     * @Serializer\Expose()
+     * @Serializer\Groups({"api-petitions-list", "api-petitions-info", "api-leader-micropetition"})
+     */
+    private $title;
+
+    /**
+     * @var string
+     * @Serializer\Expose()
+     * @Serializer\Groups({"api-petitions-list", "api-petitions-info", "api-leader-micropetition"})
+     */
+    private $description;
+
+    /**
+     * @var string
+     * @Serializer\Expose()
+     * @Serializer\Groups({"api-petitions-list", "api-petitions-info", "api-leader-micropetition"})
+     */
+    private $image;
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return Metadata
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @return Metadata
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param string $image
+     * @return Metadata
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
