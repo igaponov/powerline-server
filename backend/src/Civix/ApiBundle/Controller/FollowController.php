07c09841d529577c321d4ccda5510f62b930e453
Habibillah <habibillah@gmail.com>
2016-03-17 15:52:44 +0700

    add endpoint to follow all group members

--- backend/src/Civix/ApiBundle/Controller/FollowController.php
+++ backend/src/Civix/ApiBundle/Controller/FollowController.php
@@ -2,12 +2,18 @@
 
 namespace Civix\ApiBundle\Controller;
 
+use Civix\CoreBundle\Entity\Group;
+use Civix\CoreBundle\Entity\UserGroup;
 use Civix\CoreBundle\Event\UserEvents;
 use Civix\CoreBundle\Event\UserFollowEvent;
+use Civix\CoreBundle\Repository\UserFollowRepository;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Civix\CoreBundle\Entity\UserFollow;
 use Civix\CoreBundle\Entity\User;
 
@@ -31,19 +37,27 @@ class FollowController extends BaseController
     /**
      * @Route("/")
      * @Method("POST")
+     * @ApiDoc(
+     *     resource=true,
+     *     description="Follow a user",
+     *     statusCodes={
+     *         201="follow request success",
+     *         400="Bad request",
+     *         401="Authorization required",
+     *         405="Method Not Allowed"
+     *     }
+     * )
+     * @param Request $request
+     * @return Response
      */
     public function postAction(Request $request)
     {
-        /* @var UserFollow $follow */
-        $follow = $this->jmsDeserialization($request->getContent(), UserFollow::class,
-            ['api-follow-create']);
-        $follow->setFollower($this->getUser())->setStatus(UserFollow::STATUS_PENDING)
-            ->setUser($this->getDoctrine()->getRepository(User::class)->find($follow->getUser()->getId()))
-        ;
-        $this->getDoctrine()->getRepository(UserFollow::class)->handle($follow);
-        $this->get('civix_core.social_activity_manager')->sendUserFollowRequest($follow);
-        $event = new UserFollowEvent($follow);
-        $this->get('event_dispatcher')->dispatch(UserEvents::FOLLOWED, $event);
+        /** @var UserFollow $follow */
+        $follow = $this->jmsDeserialization($request->getContent(), UserFollow::class, ['api-follow-create']);
+
+        /** @var User $user */
+        $user = $this->getDoctrine()->getRepository(User::class)->find($follow->getUser()->getId());
+        $this->followUser($follow, $user);
 
         return $this->createJSONResponse($this->jmsSerialization($follow, ['api-follow', 'api-info']), 201);
     }
@@ -85,5 +99,75 @@ class FollowController extends BaseController
         $this->getDoctrine()->getManager()->flush($follow);
 
         return $this->createJSONResponse(null, 204);
+    }
+
+    /**
+     * @Route(
+     *     "/group/{id}",
+     *     requirements={"id"="\d+"},
+     *     name="api_follow_group_member"
+     * )
+     * @Method("POST")
+     * @ParamConverter("group", class="CivixCoreBundle:Group")
+     * @ApiDoc(
+     *     resource=true,
+     *     description="Follow group members. This api will automatically follow a group member if
+     *         group permission is public or private.",
+     *     statusCodes={
+     *         201="follow request success",
+     *         400="Bad request",
+     *         401="Authorization required",
+     *         405="Method Not Allowed"
+     *     }
+     * )
+     * @param Group $group
+     * @return Response
+     */
+    public function followGroupMember(Group $group)
+    {
+        if ($group->getTransparency() !== Group::GROUP_TRANSPARENCY_PUBLIC
+            && $group->getTransparency() !== Group::GROUP_TRANSPARENCY_PRIVATE) {
+
+            throw $this->createNotFoundException("This method just for public or private group only.");
+        }
+
+        /** @var User $loggedInUser */
+        $loggedInUser = $this->getUser();
+        $followingIds = $loggedInUser->getFollowingIds();
+
+        $members = $group->getUsers()->filter(function ($entry) use ($followingIds) {
+            return in_array($entry->getId(), $followingIds);
+        });
+
+        /** @var UserGroup $ugroup */
+        foreach ($members as $ugroup) {
+            $this->followUser(null, $ugroup->getUser());
+        }
+
+        $response = new Response('', 201);
+        return $response;
+    }
+
+    /**
+     * @param UserFollow $follow
+     * @param User $user
+     */
+    private function followUser($follow, $user)
+    {
+        if ($follow === null)
+            $follow = new UserFollow();
+
+        $follow->setFollower($this->getUser())
+            ->setStatus(UserFollow::STATUS_PENDING)
+            ->setUser($user);
+
+        /** @var UserFollowRepository $userFollowRepo */
+        $userFollowRepo = $this->getDoctrine()->getRepository(UserFollow::class);
+        $userFollowRepo->handle($follow);
+
+        $this->get('civix_core.social_activity_manager')->sendUserFollowRequest($follow);
+
+        $event = new UserFollowEvent($follow);
+        $this->get('event_dispatcher')->dispatch(UserEvents::FOLLOWED, $event);
     }
 }
