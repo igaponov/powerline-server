6aca7445926829da1d6fcd19ce83fa45e463b752
Ángel Guzmán Maeso <shakaran@gmail.com>
2016-03-21 19:17:59 +0100

    Implement a load super user data in api fixtures

---
+++ backend/src/Civix/ApiBundle/Tests/DataFixtures/ORM/LoadSuperuserData.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Civix\ApiBundle\Tests\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Civix\CoreBundle\Entity\Superuser;
+
+/**
+ * LoadSuperuserData.
+ */
+class LoadSuperuserData extends AbstractFixture implements FixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $factory = $this->container->get('security.encoder_factory');
+
+        $users [] = ['username' => 'admin'];
+        for($i = 1; $i <= 10; $i++)
+        {
+        	$users [] = ['username' => 'superuser' . $i];
+        }
+
+        foreach ($users as $data) 
+        {
+            $user = new Superuser();
+
+            $encoder = $factory->getEncoder($user);
+            $password = $encoder->encodePassword($data['username'], $user->getSalt());
+
+            $user->setUsername($data['username'])
+                ->setEmail($data['username'].'@example.com')
+                ->setPassword($password);
+            
+            $user->generateToken();
+
+            $this->addReference('superuser-'.$data['username'], $user);
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
