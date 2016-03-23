03fedd104b4707a3d6e539457ab90cc25e54a6af
Ángel Guzmán Maeso <shakaran@gmail.com>
2016-03-23 12:55:32 +0100

    Implement support for userType=group in HeaderAuthenticationListener

--- backend/src/Civix/ApiBundle/Security/Firewall/HeaderAuthenticationListener.php
+++ backend/src/Civix/ApiBundle/Security/Firewall/HeaderAuthenticationListener.php
@@ -23,27 +23,63 @@ class HeaderAuthenticationListener implements ListenerInterface
         $this->authenticationManager = $authenticationManager;
     }
 
+    /**
+     * Try to autenticate a ApiToken object.
+     * 
+     * If success it sets the token in the security context and
+     * will return TRUE.
+     * 
+     * If fails, it will return FALSE.
+     * 
+     * @param ApiToken $apiToken
+     * 
+     * @return boolean
+     */
+    private function checkAuth(ApiToken $apiToken = NULL)
+    {
+    	try
+    	{
+	    	$authToken = $this->authenticationManager->authenticate($apiToken);
+	    	
+	    	$this->securityContext->setToken($authToken);
+	    	
+	    	return TRUE;
+    	}
+    	catch (AuthenticationException $failed)
+    	{
+    		return FALSE;
+    	}
+    }
+    
     public function handle(GetResponseEvent $event)
     {
         $request = $event->getRequest();
         $apiToken = null;
 
-        if ($request->headers->has('Token')) {
+        // Plain auth based in HTTP_Token header for userType = user
+        if ($request->headers->has('Token')) 
+        {
             $apiToken = new ApiToken();
             $apiToken->setToken($request->headers->get('Token'), 'user');
-        } elseif ($request->headers->has('Authorization')) {
+        } 
+        // Check auth based in Authorization header and other userType
+        elseif ($request->headers->has('Authorization')) 
+        {
             $isTokenAuth = preg_match(
                 '/^Bearer type="(?P<type>\S+?)"\s+token="(?P<token>\S+?)"$/i',
                 $request->headers->get('Authorization'),
                 $matches
             );
-            if ($isTokenAuth) {
+            if ($isTokenAuth) 
+            {
                 $apiToken = new ApiToken();
                 $apiToken->setToken($matches['token'], $matches['type']);
             }
         }
 
-        if (!$apiToken) {
+        // Check if the token was provided in some point
+        if (!$apiToken) 
+        {
             $response = new Response();
             $response->setStatusCode(401, 'Authentication required.');
             $event->setResponse($response);
@@ -51,14 +87,21 @@ class HeaderAuthenticationListener implements ListenerInterface
             return;
         }
 
-        try {
-            $authToken = $this->authenticationManager->authenticate($apiToken);
-
-            $this->securityContext->setToken($authToken);
-        } catch (AuthenticationException $failed) {
-            $response = new Response();
-            $response->setStatusCode(401, 'Incorrect Token.');
-            $event->setResponse($response);
+        // First try to authenticate with userType = user
+        if(!$this->checkAuth($apiToken))
+        {
+        	// The userType = user has failed, we check userType = group
+        	$apiToken->setToken($request->headers->get('Token'), 'group');
+        	
+        	// If group fails, we give up (@todo pending to implement representative and superuser if needed)
+        	if(!$this->checkAuth($apiToken))
+        	{
+        		$response = new Response();
+        		$response->setStatusCode(401, 'Incorrect Token or userType.');
+        		$event->setResponse($response);
+        		
+        		return;
+        	}
         }
     }
 }
