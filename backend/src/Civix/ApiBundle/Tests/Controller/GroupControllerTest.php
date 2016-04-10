adcb8afa135d2273ea44f797618e4099bea30740
Ángel Guzmán Maeso <shakaran@gmail.com>
2016-04-10 10:44:18 +0200

    Implement a group controller test for unit testing

---
+++ backend/src/Civix/ApiBundle/Tests/Controller/GroupControllerTest.php
@@ -0,0 +1,126 @@
+<?php
+namespace Civix\ApiBundle\Tests\Controller;
+
+use Doctrine\ORM\EntityManager;
+
+use Civix\CoreBundle\Tests\DataFixtures\ORM\LoadGroupData;
+use Civix\CoreBundle\Tests\DataFixtures\ORM\LoadUserData;
+use Civix\CoreBundle\Tests\DataFixtures\ORM\LoadUserGroupData;
+use Civix\ApiBundle\Tests\DataFixtures\ORM\LoadSuperuserData;
+use Civix\ApiBundle\Tests\WebTestCase;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class GroupControllerTest extends WebTestCase
+{
+	const API_GROUP_IS_OWNER_ENDPOINT   = '/api/groups/is-owner';
+	const API_GROUP_IS_MEMBER_ENDPOINT  = '/api/groups/is-member';
+	const API_GROUP_IS_MANAGER_ENDPOINT = '/api/groups/is-manager';
+	
+	/**
+	 * @var \Doctrine\ORM\EntityManager
+	 */
+	private $em;
+	
+	private $client = null;
+
+	public function setUp()
+	{
+		// Creates a initial client
+		$this->client = static::createClient();
+
+		/** @var AbstractExecutor $fixtures */
+		$fixtures = $this->loadFixtures([
+				LoadUserData::class,
+				LoadGroupData::class,
+				LoadUserGroupData::class,
+				LoadSuperuserData::class
+		]);
+		
+		$reference = $fixtures->getReferenceRepository();
+		
+		$this->em = $this->getContainer()->get('doctrine')->getManager();
+		
+		$this->group = $reference->getReference('group');
+		
+		$this->mobile1 = $reference->getReference('followertest');
+		
+		$this->mobile1_token = $this->getUserToken($this->mobile1->getUsername(), $this->mobile1->getUsername());
+	}
+
+	public function tearDown()
+	{
+		// Creates a initial client
+		$this->client = NULL;
+	}
+	
+	/**
+	 * group api
+	 */
+	public function testGroupIsOwner()
+	{
+		$this->assertNotEmpty($this->mobile1_token, 'Login token should not empty');
+		
+		// Create a request scope context that allows serialize the question object
+		$container = $this->getContainer();
+		
+		$request = Request::create('/');
+		
+		$request->setSession( new Session() );
+		$container->enterScope('request');
+		
+		$container->set('request', $request, 'request');
+		
+		$content = [];
+		
+		// Test is owner endpoint for failed result
+		$end_point = self::API_GROUP_IS_OWNER_ENDPOINT . '/' . $this->group->getId();
+		
+		$this->client->request('GET', $end_point, [], [], ['HTTP_Token' => $this->mobile1_token], $content);
+		
+		$request = $this->client->getRequest();
+		
+		$response = $this->client->getResponse();
+		
+		$content = json_decode($response->getContent());
+
+		$this->assertNotEmpty($content->error, 'The user is not owner of the group');
+		
+		$this->assertEquals(
+				404,
+				$response->getStatusCode(),
+				'Should be a 404 response'
+				);
+		
+		$this->assertNotEmpty($response->headers->get('Access-Control-Allow-Origin'),
+				'Should return cors headers');
+		
+		// Set as owner of the group the mobile user
+		$this->group->setOwner($this->mobile1);
+
+		$this->em->flush($this->group);
+		
+		// Test is owner endpoint for failed result
+		$end_point = self::API_GROUP_IS_OWNER_ENDPOINT . '/' . $this->group->getId();
+		
+		$this->client->request('GET', $end_point, [], [], ['HTTP_Token' => $this->mobile1_token], $content);
+		
+		$request = $this->client->getRequest();
+		
+		$response = $this->client->getResponse();
+		
+		$content = json_decode($response->getContent());
+		
+		$this->assertNotEmpty($content, 'true');
+		
+		$this->assertEquals(
+				204,
+				$response->getStatusCode(),
+				'Should be a 204 response'
+				);
+		
+		$this->assertNotEmpty($response->headers->get('Access-Control-Allow-Origin'),
+				'Should return cors headers');
+	}
+}
