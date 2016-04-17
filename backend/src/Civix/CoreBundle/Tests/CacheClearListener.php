9cd7d26057725ff437c1746b57a8e35fe526a46d
Igor Gaponov <jiminy96@gmail.com>
2016-04-17 17:40:32 +0700

    Workaround for a bug with doctrine fixtures

---
+++ backend/src/Civix/CoreBundle/Tests/CacheClearListener.php
@@ -0,0 +1,111 @@
+<?php
+namespace Civix\CoreBundle\Tests;
+
+use Exception;
+use PHPUnit_Framework_AssertionFailedError;
+use PHPUnit_Framework_Test;
+use PHPUnit_Framework_TestSuite;
+
+/**
+ * https://github.com/doctrine/data-fixtures/issues/120
+ *
+ * @todo Delete after upgrade to Doctrine v2.4
+ */
+class CacheClearListener implements \PHPUnit_Framework_TestListener
+{
+
+    /**
+     * An error occurred.
+     *
+     * @param  PHPUnit_Framework_Test $test
+     * @param  Exception $e
+     * @param  float $time
+     */
+    public function addError(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+        // TODO: Implement addError() method.
+    }
+
+    /**
+     * A failure occurred.
+     *
+     * @param  PHPUnit_Framework_Test $test
+     * @param  PHPUnit_Framework_AssertionFailedError $e
+     * @param  float $time
+     */
+    public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time)
+    {
+        // TODO: Implement addFailure() method.
+    }
+
+    /**
+     * Incomplete test.
+     *
+     * @param  PHPUnit_Framework_Test $test
+     * @param  Exception $e
+     * @param  float $time
+     */
+    public function addIncompleteTest(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+        // TODO: Implement addIncompleteTest() method.
+    }
+
+    /**
+     * Skipped test.
+     *
+     * @param  PHPUnit_Framework_Test $test
+     * @param  Exception $e
+     * @param  float $time
+     * @since  Method available since Release 3.0.0
+     */
+    public function addSkippedTest(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+        // TODO: Implement addSkippedTest() method.
+    }
+
+    /**
+     * A test suite started.
+     *
+     * @param  PHPUnit_Framework_TestSuite $suite
+     * @since  Method available since Release 2.2.0
+     */
+    public function startTestSuite(PHPUnit_Framework_TestSuite $suite)
+    {
+        // TODO: Implement startTestSuite() method.
+    }
+
+    /**
+     * A test suite ended.
+     *
+     * @param  PHPUnit_Framework_TestSuite $suite
+     * @since  Method available since Release 2.2.0
+     */
+    public function endTestSuite(PHPUnit_Framework_TestSuite $suite)
+    {
+        // TODO: Implement endTestSuite() method.
+    }
+
+    /**
+     * A test started.
+     *
+     * @param  PHPUnit_Framework_Test $test
+     */
+    public function startTest(PHPUnit_Framework_Test $test)
+    {
+        // TODO: Implement startTest() method.
+    }
+
+    /**
+     * A test ended.
+     *
+     * @param  PHPUnit_Framework_Test $test
+     * @param  float $time
+     */
+    public function endTest(PHPUnit_Framework_Test $test, $time)
+    {
+        $pattern = __DIR__.'/../../../../app/cache/test/test_*.ser';
+        foreach (glob($pattern) as $item) {
+            unlink($item);
+        }
+    }
+}
