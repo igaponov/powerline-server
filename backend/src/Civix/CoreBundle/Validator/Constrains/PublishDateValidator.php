5d5f37cdd3c8e09c41fd4dd2ecb9b3eb3b29d918
Igor Gaponov <jiminy96@gmail.com>
2016-07-05 00:32:38 +0500

    Add PublishDate validator

---
+++ backend/src/Civix/CoreBundle/Validator/Constrains/PublishDateValidator.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Civix\CoreBundle\Validator\Constrains;
+
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+class PublishDateValidator extends ConstraintValidator
+{
+    public function validate($value, Constraint $constraint)
+    {
+        if (!$constraint instanceof PublishDate) {
+            throw new UnexpectedTypeException($constraint, PublishDate::class);
+        }
+
+        if (!is_object($value)) {
+            throw new UnexpectedTypeException($value, 'object');
+        }
+
+        $propertyAccessor = new PropertyAccessor();
+        $date = $propertyAccessor->getValue($value, $constraint->property);
+
+        if ($date && $date < new \DateTime()) {
+            $this->context->addViolation(
+                $constraint->message,
+                ['{object}' => $constraint->objectName ? : substr(strrchr(get_class($value), "\\"), 1)]
+            );
+        }
+    }
+}
