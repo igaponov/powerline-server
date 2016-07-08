167bdd3280aa0b3e2b4e9fefe3282d1b90a467f6
Igor Gaponov <jiminy96@gmail.com>
2016-07-08 18:14:58 +0500

    Update poll fixtures

--- backend/src/Civix/CoreBundle/Tests/DataFixtures/ORM/Group/LoadGroupQuestionData.php
+++ backend/src/Civix/CoreBundle/Tests/DataFixtures/ORM/Group/LoadGroupQuestionData.php
@@ -1,64 +1,26 @@
 <?php
 namespace Civix\CoreBundle\Tests\DataFixtures\ORM\Group;
 
-use Civix\CoreBundle\Entity\Group;
 use Civix\CoreBundle\Entity\Poll\Option;
 use Civix\CoreBundle\Entity\Poll\Question\Group as GroupQuestion;
-use Civix\CoreBundle\Entity\Stripe\CustomerGroup;
-use Civix\CoreBundle\Tests\DataFixtures\ORM\LoadGroupFollowerTestData;
+use Civix\CoreBundle\Tests\DataFixtures\ORM\LoadGroupData;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class LoadGroupQuestionData extends AbstractFixture implements ContainerAwareInterface, DependentFixtureInterface
+class LoadGroupQuestionData extends AbstractFixture implements DependentFixtureInterface
 {
-    /**
-     * @var ContainerInterface
-     */
-    private $container;
-
-    /** @var ObjectManager */
-    private $manager;
-
-    public function setContainer(ContainerInterface $container = null)
-    {
-        $this->container = $container;
-    }
-
     public function load(ObjectManager $manager)
     {
-        $this->manager = $manager;
-
-        $this->addReference(
-            'group-question',
-            $this->createQuestion($this->getReference('group'))
-        );
-        $this->createQuestion($this->getReference('group'));
-        $this->createQuestion($this->getReference('group'));
-        $this->addReference(
-            'testfollowsecretgroups-question', 
-            $this->createQuestion($this->getReference('testfollowsecretgroups'))
-        );
-        $this->createQuestion($this->getReference('testfollowprivategroups'));
-    }
-
-    public function getDependencies()
-    {
-        return [LoadGroupFollowerTestData::class];
-    }
-
-    /**
-     * @param $group
-     * @return CustomerGroup
-     */
-    private function createQuestion($group)
-    {
         $faker = Factory::create();
+
+        $group1 = $this->getReference('group_1');
+        $group2 = $this->getReference('group_2');
+        $group3 = $this->getReference('group_3');
+
         $question = new GroupQuestion();
-        $question->setUser($group);
+        $question->setUser($group1);
         $question->setSubject('subj '.$faker->sentence);
         $question->setExpireAt(new \DateTime('+1 day'));
         for ($i = 0; $i < 2; $i++) {
@@ -66,10 +28,54 @@ class LoadGroupQuestionData extends AbstractFixture implements ContainerAwareInt
             $option->setValue('val '.$faker->word);
             $question->addOption($option);
         }
+        $manager->persist($question);
+        $this->addReference('group_question_1', $question);
 
-        $this->manager->persist($question);
-        $this->manager->flush();
+        // expired
+        $question = new GroupQuestion();
+        $question->setUser($group2);
+        $question->setSubject('subj '.$faker->sentence);
+        $question->setExpireAt(new \DateTime('-1 day'));
+        for ($i = 0; $i < 2; $i++) {
+            $option = new Option();
+            $option->setValue('val '.$faker->word);
+            $question->addOption($option);
+        }
+        $manager->persist($question);
+        $this->addReference('group_question_2', $question);
 
-        return $question;
+        // 3 options
+        $question = new GroupQuestion();
+        $question->setUser($group3);
+        $question->setSubject('subj '.$faker->sentence);
+        $question->setExpireAt(new \DateTime('+1 week'));
+        for ($i = 0; $i < 3; $i++) {
+            $option = new Option();
+            $option->setValue('val '.$faker->word);
+            $question->addOption($option);
+        }
+        $manager->persist($question);
+        $this->addReference('group_question_3', $question);
+
+        // published
+        $question = new GroupQuestion();
+        $question->setUser($group3);
+        $question->setSubject('subj '.$faker->sentence);
+        $question->setExpireAt(new \DateTime('+1 month'));
+        $question->setPublishedAt(new \DateTime('-1 day'));
+        for ($i = 0; $i < 2; $i++) {
+            $option = new Option();
+            $option->setValue('val '.$faker->word);
+            $question->addOption($option);
+        }
+        $manager->persist($question);
+        $this->addReference('group_question_4', $question);
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [LoadGroupData::class];
     }
 }
