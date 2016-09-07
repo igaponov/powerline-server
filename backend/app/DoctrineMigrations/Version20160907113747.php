d3b548033ba6861661dace68d153959b0a0176fb
Igor Gaponov <jiminy96@gmail.com>
2016-09-07 20:48:32 +0700

    Fix migration 20160907113747 for old mysql version

--- backend/app/DoctrineMigrations/Version20160907113747.php
+++ backend/app/DoctrineMigrations/Version20160907113747.php
@@ -19,6 +19,7 @@ class Version20160907113747 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE groups_fields_values DROP FOREIGN KEY FK_7984E7FAA76ED395');
+        $this->addSql('ALTER TABLE groups_fields_values DROP FOREIGN KEY FK_7984E7FA443707B0');
         $this->addSql('
             DELETE gfv FROM groups_fields_values gfv 
             LEFT JOIN groups_fields gf ON gfv.field_id = gf.id
@@ -27,6 +28,7 @@ class Version20160907113747 extends AbstractMigration
         ');
         $this->addSql('ALTER TABLE groups_fields_values CHANGE field_id field_id INT NOT NULL, CHANGE user_id user_id INT NOT NULL');
         $this->addSql('ALTER TABLE groups_fields_values ADD CONSTRAINT FK_7984E7FAA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE groups_fields_values ADD CONSTRAINT FK_7984E7FA443707B0 FOREIGN KEY (`field_id`) REFERENCES `groups_fields` (`id`) ON DELETE CASCADE');
     }
 
     /**
