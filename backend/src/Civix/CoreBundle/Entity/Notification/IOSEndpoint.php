3d5c6f52c7643dfbfaa5157a59aa59b89501fcf9
Igor Gaponov <jiminy96@gmail.com>
2016-10-05 21:52:55 +0700

    Fix IOS push message structure fox sandbox mode

--- backend/src/Civix/CoreBundle/Entity/Notification/IOSEndpoint.php
+++ backend/src/Civix/CoreBundle/Entity/Notification/IOSEndpoint.php
@@ -11,13 +11,23 @@ class IOSEndpoint extends AbstractEndpoint
 {
     public function getPlatformMessage($title, $message, $type, $entityData, $image, $badge = null)
     {
-        return json_encode(array('APNS' => json_encode(array('aps' => array(
-            'alert' => $message,
-            'entity' => json_encode($entityData),
-            'type' => $type,
-            'sound' => 'default',
-            'title' => $title,
-            'image' => $image,
-        )))));
+        $payload = json_encode(
+            array(
+                'aps' => array(
+                    'alert' => $message,
+                    'entity' => json_encode($entityData),
+                    'type' => $type,
+                    'sound' => 'default',
+                    'title' => $title,
+                    'image' => $image,
+                )
+            )
+        );
+
+        return json_encode(array(
+            'default' => $message,
+            'APNS' => $payload,
+            'APNS_SANDBOX' => $payload,
+        ));
     }
 }
