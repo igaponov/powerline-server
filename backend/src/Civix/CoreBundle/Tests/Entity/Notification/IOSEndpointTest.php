3d43633cd81343ae716fbeea4529f26359f385cd
Igor Gaponov <jiminy96@gmail.com>
2016-10-08 15:17:43 +0700

    Fix IOS endpoint test

--- backend/src/Civix/CoreBundle/Tests/Entity/Notification/IOSEndpointTest.php
+++ backend/src/Civix/CoreBundle/Tests/Entity/Notification/IOSEndpointTest.php
@@ -13,7 +13,7 @@ class IOSEndpointTest extends \PHPUnit_Framework_TestCase
     {
         $endpoint = new IOSEndpoint;
         $this->assertEquals(
-            '{"default":"test_message","APNS":"{\"aps\":{\"alert\":{\"title\":\"test_message\",\"body\":\"test_message\"},\"entity\":\"null\",\"type\":\"test_type\",\"category\":\"test_type\",\"sound\":\"default\",\"title\":\"test_title\",\"image\":null,\"badge\":5,\"additionalData\":{\"badgeCount\":5}}}","APNS_SANDBOX":"{\"aps\":{\"alert\":{\"title\":\"test_message\",\"body\":\"test_message\"},\"entity\":\"null\",\"type\":\"test_type\",\"category\":\"test_type\",\"sound\":\"default\",\"title\":\"test_title\",\"image\":null,\"badge\":5,\"additionalData\":{\"badgeCount\":5}}}"}',
+            '{"default":"test_message","APNS":"{\"aps\":{\"alert\":{\"title\":\"test_title\",\"body\":\"test_message\"},\"entity\":\"null\",\"type\":\"test_type\",\"category\":\"test_type\",\"sound\":\"default\",\"title\":\"test_title\",\"image\":null,\"badge\":5,\"additionalData\":{\"badgeCount\":5}}}","APNS_SANDBOX":"{\"aps\":{\"alert\":{\"title\":\"test_title\",\"body\":\"test_message\"},\"entity\":\"null\",\"type\":\"test_type\",\"category\":\"test_type\",\"sound\":\"default\",\"title\":\"test_title\",\"image\":null,\"badge\":5,\"additionalData\":{\"badgeCount\":5}}}"}',
             $endpoint->getPlatformMessage('test_title', 'test_message', 'test_type', null, null, 5)
         );
     }
