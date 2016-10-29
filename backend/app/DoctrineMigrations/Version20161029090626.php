ec829177e8db51449433c0a35dcac48ab6215152
Igor Gaponov <jiminy96@gmail.com>
2016-10-29 16:08:37 +0700

    Migration to delete username from group entity

---
+++ backend/app/DoctrineMigrations/Version20161029090626.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20161029090626 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE groups DROP username, DROP password, DROP salt, DROP token');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE groups ADD username VARCHAR(255) NOT NULL, ADD password VARCHAR(255) NOT NULL, ADD salt VARCHAR(255) NOT NULL, ADD token VARCHAR(255) DEFAULT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_F06D3970F85E0677 ON groups (username)');
+    }
+}
