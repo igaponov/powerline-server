d66cbfe9b8fe6c280987f2511fe93087334c5ffa
Igor Gaponov <jiminy96@gmail.com>
2016-12-16 23:07:15 +0700

    Fix metadata listener for image urls

--- backend/src/Civix/ApiBundle/EventListener/MetadataListener.php
+++ backend/src/Civix/ApiBundle/EventListener/MetadataListener.php
@@ -1,6 +1,7 @@
 <?php
 namespace Civix\ApiBundle\EventListener;
 
+use Civix\CoreBundle\Entity\Metadata;
 use Civix\CoreBundle\Entity\Post;
 use Civix\CoreBundle\Entity\UserPetition;
 use Civix\CoreBundle\Service\HTMLMetadataParser;
@@ -32,10 +33,18 @@ class MetadataListener
         if (preg_match(self::PATTERN, $entity->getBody(), $matches)) {
             $url = $matches[1];
             $response = $this->getResponse($url);
-            $metadata = $this->parser->parse($response->getBody());
-            if ($metadata->getTitle()) {
-                $metadata->setUrl($url);
+            $header = $response->getHeaderLine('content-type');
+            if ($header && strpos($header, 'image') === 0) {
+                $metadata = new Metadata();
+                $metadata->setImage($url)
+                    ->setUrl($url);
                 $entity->setMetadata($metadata);
+            } else {
+                $metadata = $this->parser->parse($response->getBody());
+                if ($metadata->getTitle()) {
+                    $metadata->setUrl($url);
+                    $entity->setMetadata($metadata);
+                }
             }
         }
     }
