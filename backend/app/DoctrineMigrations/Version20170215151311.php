7f87cf5868a4704d78f1613cee60bb8230512bfb
Igor Gaponov <jiminy96@gmail.com>
2017-02-15 22:25:50 +0700

    Add a migration for automatic boost flag

---
+++ backend/app/DoctrineMigrations/Version20170215151311.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170215151311 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user_posts ADD automatic_boost TINYINT(1) DEFAULT \'1\' NOT NULL');
+        $this->addSql('ALTER TABLE user_petitions ADD automatic_boost TINYINT(1) DEFAULT \'1\' NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user_petitions DROP automatic_boost');
+        $this->addSql('ALTER TABLE user_posts DROP automatic_boost');
+    }
+}
