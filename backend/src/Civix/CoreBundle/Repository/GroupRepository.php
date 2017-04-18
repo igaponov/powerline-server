d6c75193282f15b23f9d9ec60d82d98f8578d167
Igor Gaponov <jiminy96@gmail.com>
2017-04-18 16:47:05 +0700

    Cleanup group repository

--- backend/src/Civix/CoreBundle/Repository/GroupRepository.php
+++ backend/src/Civix/CoreBundle/Repository/GroupRepository.php
@@ -10,22 +10,6 @@ use Civix\CoreBundle\Entity\UserGroup;
 class GroupRepository extends EntityRepository
 {
     /**
-     *
-     * @return array
-     */
-    public function getGroupsByUser()
-    {
-        $queryBuilder = $this->getEntityManager()->createQueryBuilder();
-
-        return $queryBuilder
-                ->select('gr')
-                ->from('CivixCoreBundle:Group', 'gr')
-                ->getQuery()
-                ->getResult();
-    }
-
-    /**
-     *
      * @param User $user
      * @return \Doctrine\ORM\Query
      */
@@ -47,8 +31,8 @@ class GroupRepository extends EntityRepository
     }
 
     /**
-     * 
      * @param User $user
+     * @return Group[]
      */
     public function getPopularGroupsByUser(User $user)
     {
@@ -80,8 +64,9 @@ class GroupRepository extends EntityRepository
      * Fetch the groups of common type from 7 days ago to the current
      * moment and check if the current user belongs in each groups
      * for display as groups news results.
-     * 
+     *
      * @param User $user
+     * @return Group[]
      */
     public function getNewGroupsByUser(User $user)
     {
@@ -112,9 +97,9 @@ class GroupRepository extends EntityRepository
     }
 
     /**
-     * 
-     * @param unknown $type
+     * @param int $type
      * @param string $order
+     * @return \Doctrine\ORM\QueryBuilder
      */
     public function getQueryGroupOrderedById($type = Group::GROUP_TYPE_COMMON, $order = 'DESC')
     {
@@ -125,57 +110,9 @@ class GroupRepository extends EntityRepository
     }
 
     /**
-     * 
-     * @param Group $countryGroup
-     */
-    public function getQueryCountryGroupChildren(Group $countryGroup)
-    {
-        return $this->createQueryBuilder('g')
-            ->where('g.parent = :parent')
-            ->setParameter('parent', $countryGroup)
-        ;
-    }
-
-    /**
-     * 
-     * @param Group $group
-     */
-    public function removeGroup(Group $group)
-    {
-        $this->getEntityManager()
-            ->createQueryBuilder()
-            ->update('CivixCoreBundle:Poll\Question\Group g')
-            ->set('g.user', 'NULL')
-            ->where('g.user = :groupId')
-            ->setParameter('groupId', $group->getId())
-            ->getQuery()
-            ->execute();
-
-        $this->getEntityManager()
-            ->createQueryBuilder()
-            ->update('CivixCoreBundle:Activity a')
-            ->set('a.group', 'NULL')
-            ->where('a.group = :groupId')
-            ->setParameter('groupId', $group->getId())
-            ->getQuery()
-            ->execute();
-
-        $this->getEntityManager()->getConnection()
-                ->delete('users_groups', array('group_id' => $group->getId()));
-
-        $this->getEntityManager()
-            ->createQueryBuilder()
-            ->delete('CivixCoreBundle:Group g')
-            ->where('g.id = :groupId')
-            ->setParameter('groupId', $group->getId())
-            ->getQuery()
-            ->execute();
-    }
-
-    /**
-     * 
-     * @param unknown $id
-     * @param unknown $type
+     * @param integer $id
+     * @param integer $type
+     * @return Group|null|object
      */
     public function getGroupByIdAndType($id, $type = Group::GROUP_TYPE_COMMON)
     {
@@ -186,24 +123,9 @@ class GroupRepository extends EntityRepository
     }
 
     /**
-     * 
-     * @param unknown $state
-     */
-    public function getLocalGroupsByState($state)
-    {
-        return $this->createQueryBuilder('g')
-                ->where('g.groupType = :type')
-                ->andWhere('g.localState = :state')
-                ->setParameter('type', Group::GROUP_TYPE_LOCAL)
-                ->setParameter('state', $state)
-                ->getQuery()->getResult()
-        ;
-    }
-
-    /**
-     *
-     * @param unknown $id
-     * @param unknown $representativeId
+     * @param integer $id
+     * @param integer $representativeId
+     * @return Group|null
      */
     public function getLocalGroupForRepr($id, $representativeId)
     {
