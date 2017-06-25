c918754f4097af72e331a310fa09536bdad9dbfc
Igor Gaponov <jiminy96@gmail.com>
2017-06-25 23:38:19 +0300

    Add Tag entity

---
+++ backend/src/Civix/CoreBundle/Entity/Group/Tag.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Civix\CoreBundle\Entity\Group;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Table(name="group_tags", options={"charset"="utf8mb4", "collate"="utf8mb4_unicode_ci", "row_format"="DYNAMIC"})
+ * @ORM\Entity()
+ * @Serializer\ExclusionPolicy("ALL")
+ */
+class Tag
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @Serializer\Expose()
+     * @Serializer\Groups({"group-tag"})
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank()
+     * @Serializer\Expose()
+     * @Serializer\Groups({"group-tag"})
+     */
+    private $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
