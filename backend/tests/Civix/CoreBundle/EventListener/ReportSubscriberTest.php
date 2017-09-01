5def5cdd318308c850fe1dc4a2d456e4a9e1f987
Igor Gaponov <jiminy96@gmail.com>
2017-09-01 16:14:59 +0200

    Add a test for ReportSubscriber

--- backend/tests/Civix/CoreBundle/EventListener/ReportSubscriberTest.php
+++ backend/tests/Civix/CoreBundle/EventListener/ReportSubscriberTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Civix\CoreBundle\EventListener;
 
+use Civix\CoreBundle\Entity\Post;
 use Civix\CoreBundle\Entity\Report\UserReport;
 use Civix\CoreBundle\Entity\User;
+use Civix\CoreBundle\Event\PostEvent;
 use Civix\CoreBundle\Event\UserEvent;
 use Civix\CoreBundle\EventListener\ReportSubscriber;
 use Civix\CoreBundle\Repository\Report\UserReportRepository;
@@ -30,5 +32,27 @@ class ReportSubscriberTest extends TestCase
         $subscriber = new ReportSubscriber($em);
         $event = new UserEvent($user);
         $subscriber->createUserReport($event);
+    }
+
+    public function testUpdateKarmaCreatePost(): void
+    {
+        $user = new User();
+        $post = new Post();
+        $post->setUser($user);
+        $repository = $this->getMockBuilder(UserReportRepository::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['updateUserReportKarma'])
+            ->getMock();
+        $repository->expects($this->once())
+            ->method('updateUserReportKarma')
+            ->with($user);
+        $em = $this->createMock(EntityManagerInterface::class);
+        $em->expects($this->once())
+            ->method('getRepository')
+            ->with(UserReport::class)
+            ->willReturn($repository);
+        $subscriber = new ReportSubscriber($em);
+        $event = new PostEvent($post);
+        $subscriber->updateKarmaCreatePost($event);
     }
 }
