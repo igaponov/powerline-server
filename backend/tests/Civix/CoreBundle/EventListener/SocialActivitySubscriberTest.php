aee905db7d774cd8f9e7abf0b54164d30113c465
Igor Gaponov <jiminy96@gmail.com>
2017-09-04 01:41:12 +0200

    Upd SocialActivitySubscriber test with petition's methods

--- backend/tests/Civix/CoreBundle/EventListener/SocialActivitySubscriberTest.php
+++ backend/tests/Civix/CoreBundle/EventListener/SocialActivitySubscriberTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Civix\CoreBundle\EventListener;
 
 use Civix\CoreBundle\Entity\Post;
+use Civix\CoreBundle\Entity\UserPetition;
 use Civix\CoreBundle\Event\PostEvent;
+use Civix\CoreBundle\Event\UserPetitionEvent;
 use Civix\CoreBundle\EventListener\SocialActivitySubscriber;
 use Civix\CoreBundle\Service\SocialActivityManager;
 use PHPUnit\Framework\TestCase;
@@ -20,6 +22,18 @@ class SocialActivitySubscriberTest extends TestCase
             ->with($post);
         $subscriber = new SocialActivitySubscriber($manager);
         $subscriber->noticePostCreated($event);
+    }
+
+    public function testNoticeUserPetitionCreated()
+    {
+        $petition = new UserPetition();
+        $event = new UserPetitionEvent($petition);
+        $manager = $this->getSocialActivityManagerMock(['noticeUserPetitionCreated']);
+        $manager->expects($this->once())
+            ->method('noticeUserPetitionCreated')
+            ->with($petition);
+        $subscriber = new SocialActivitySubscriber($manager);
+        $subscriber->noticeUserPetitionCreated($event);
     }
 
     /**
