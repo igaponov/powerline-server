2690ff5039f6fce65699876129a6f7d4f6bcabeb
Igor Gaponov <jiminy96@gmail.com>
2017-09-04 12:47:15 +0200

    Get rid of warnings in UserPetitionCreateType

--- backend/src/Civix/ApiBundle/Form/Type/UserPetitionCreateType.php
+++ backend/src/Civix/ApiBundle/Form/Type/UserPetitionCreateType.php
@@ -4,15 +4,16 @@ namespace Civix\ApiBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserPetitionCreateType extends AbstractType
 {
-    public function getBlockPrefix()
+    public function getBlockPrefix(): string
     {
         return '';
     }
 
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('title')
@@ -28,7 +29,14 @@ class UserPetitionCreateType extends AbstractType
         ;
     }
 
-    public function getParent()
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'validation_groups' => 'create',
+        ]);
+    }
+
+    public function getParent(): string
     {
         return UserPetitionUpdateType::class;
     }
