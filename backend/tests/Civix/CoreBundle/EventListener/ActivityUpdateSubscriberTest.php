50c843a58c4abe4bf03691972d31ca5445b3f00f
Igor Gaponov <jiminy96@gmail.com>
2017-09-12 17:35:57 +0200

    Add a test for ActivityUpdateSubscriberTest::testUpdateResponsesComment

--- backend/tests/Civix/CoreBundle/EventListener/ActivityUpdateSubscriberTest.php
+++ backend/tests/Civix/CoreBundle/EventListener/ActivityUpdateSubscriberTest.php
@@ -2,8 +2,12 @@
 
 namespace Tests\Civix\CoreBundle\EventListener;
 
+use Civix\CoreBundle\Entity\BaseComment;
+use Civix\CoreBundle\Entity\Poll;
 use Civix\CoreBundle\Entity\Post;
+use Civix\CoreBundle\Entity\User;
 use Civix\CoreBundle\Entity\UserPetition;
+use Civix\CoreBundle\Event\CommentEvent;
 use Civix\CoreBundle\Event\PostEvent;
 use Civix\CoreBundle\Event\UserPetitionEvent;
 use Civix\CoreBundle\EventListener\ActivityUpdateSubscriber;
@@ -34,6 +38,33 @@ class ActivityUpdateSubscriberTest extends TestCase
             ->with($petition);
         $subscriber = new ActivityUpdateSubscriber($activityUpdate);
         $subscriber->publishUserPetitionToActivity($event);
+    }
+
+    /**
+     * @param BaseComment $comment
+     * @param string $method
+     * @dataProvider getComments
+     */
+    public function testUpdateResponsesComment(BaseComment $comment, string $method)
+    {
+        $event = new CommentEvent($comment);
+        $activityUpdate = $this->getActivityUpdateMock([$method]);
+        $activityUpdate->expects($this->once())
+            ->method($method)
+            ->with($comment->getCommentedEntity());
+        $subscriber = new ActivityUpdateSubscriber($activityUpdate);
+        $subscriber->updateResponsesComment($event);
+    }
+
+    public function getComments()
+    {
+        $user = new User();
+
+        return [
+            [(new Poll\Comment($user))->setQuestion(new Poll\Question\Group()), 'updateResponsesQuestion'],
+            [(new UserPetition\Comment($user))->setPetition(new UserPetition()), 'updateResponsesPetition'],
+            [(new Post\Comment($user))->setPost(new Post()), 'updateResponsesPost'],
+        ];
     }
 
     /**
