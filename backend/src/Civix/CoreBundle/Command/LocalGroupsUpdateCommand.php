9cb5a9473e6e11324c2ec22ddb980b2fa3bf30ca
Igor Gaponov <jiminy96@gmail.com>
2017-09-30 23:57:29 +0200

    Add a possibility to update local groups for particular users

--- backend/src/Civix/CoreBundle/Command/LocalGroupsUpdateCommand.php
+++ backend/src/Civix/CoreBundle/Command/LocalGroupsUpdateCommand.php
@@ -4,6 +4,7 @@ namespace Civix\CoreBundle\Command;
 use Civix\CoreBundle\Entity\User;
 use Doctrine\ORM\Internal\Hydration\IterableResult;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -11,16 +12,26 @@ class LocalGroupsUpdateCommand extends ContainerAwareCommand
 {
     protected function configure(): void
     {
-        $this->setName('civix:local_groups:update');
+        $this->setName('civix:local_groups:update')
+            ->addArgument('users', InputArgument::IS_ARRAY|InputArgument::OPTIONAL, 'User ids');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): void
     {
-        /** @var IterableResult $users */
-        $users = $this->getContainer()->get('doctrine.orm.entity_manager')
-            ->getRepository(User::class)
-            ->createQueryBuilder('u')
-            ->getQuery()->iterate();
+        if ($input->getArgument('users')) {
+            $users = array_map(function (User $user) {
+                return [$user];
+            }, (array)$this->getContainer()->get('doctrine.orm.entity_manager')
+                ->getRepository(User::class)
+                ->findBy(['id' => $input->getArgument('users')])
+            );
+        } else {
+            /** @var IterableResult $users */
+            $users = $this->getContainer()->get('doctrine.orm.entity_manager')
+                ->getRepository(User::class)
+                ->createQueryBuilder('u')
+                ->getQuery()->iterate();
+        }
         $manager = $this->getContainer()->get('civix_core.service.user_local_group_manager');
         foreach ($users as $user) {
             /** @var User[] $user */
