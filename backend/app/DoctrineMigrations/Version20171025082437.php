29e1a7ae34209e7c9500f73d071c2c95725f1070
Igor Gaponov <jiminy96@gmail.com>
2017-10-25 13:25:18 +0500

    Add a migration for post/petition image

---
+++ backend/app/DoctrineMigrations/Version20171025082437.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171025082437 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user_posts ADD image_name VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user_petitions ADD image_name VARCHAR(255) DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user_petitions DROP image_name');
+        $this->addSql('ALTER TABLE user_posts DROP image_name');
+    }
+}
