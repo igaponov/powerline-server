5ed725aa10a33fb94ec3bbea4f40eea518c1b084
Igor Gaponov <jiminy96@gmail.com>
2017-11-16 22:07:00 +0500

    Get rid of warnings in Charge

--- backend/src/Civix/CoreBundle/Entity/Stripe/Charge.php
+++ backend/src/Civix/CoreBundle/Entity/Stripe/Charge.php
@@ -12,6 +12,8 @@ use Doctrine\ORM\Mapping as ORM;
 class Charge
 {
     /**
+     * @var int
+     *
      * @ORM\Id
      * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
@@ -19,42 +21,57 @@ class Charge
     private $id;
 
     /**
+     * @var string
+     *
      * @ORM\Column(name="stripe_id", type="string")
      */
     private $stripeId;
 
     /**
+     * @var string
+     *
      * @ORM\Column(name="status", type="string")
      */
     private $status;
 
     /**
+     * @var int
+     *
      * @ORM\Column(name="amount", type="integer")
      */
     private $amount;
 
     /**
+     * @var int
+     *
      * @ORM\Column(name="application_fee", type="integer", nullable=true)
      */
     private $applicationFee;
 
     /**
+     * @var string
+     *
      * @ORM\Column(name="currency", type="string", length=3)
      */
     private $currency;
 
     /**
+     * @var string
+     *
      * @ORM\Column(name="receipt_number", type="string", nullable=true)
      */
     private $receiptNumber;
 
     /**
+     * @var int
+     *
      * @ORM\Column(name="created", type="integer")
      */
     private $created;
 
     /**
      * @var Question
+     *
      * @ORM\ManyToOne(targetEntity="Civix\CoreBundle\Entity\Poll\Question")
      * @ORM\JoinColumn(onDelete="CASCADE")
      */
@@ -62,6 +79,7 @@ class Charge
 
     /**
      * @var Customer
+     *
      * @ORM\JoinColumn(name="from_customer", nullable=false)
      * @ORM\ManyToOne(targetEntity="Civix\CoreBundle\Entity\Stripe\Customer")
      * @ORM\JoinColumn(onDelete="CASCADE", nullable=false)
@@ -70,14 +88,14 @@ class Charge
 
     /**
      * @var Account|null
+     *
      * @ORM\JoinColumn(name="to_account")
      * @ORM\ManyToOne(targetEntity="Civix\CoreBundle\Entity\Stripe\Account")
      * @ORM\JoinColumn(onDelete="CASCADE")
      */
     private $toAccount;
 
-    public function __construct(Customer $customer, Account $account = null,
-                                Question $question = null)
+    public function __construct(Customer $customer, Account $account = null, Question $question = null)
     {
         $this->fromCustomer = $customer;
         $this->toAccount = $account;
@@ -85,10 +103,10 @@ class Charge
     }
 
     /**
-     * @param \Stripe\Charge $sc
+     * @param \Stripe\Charge|\stdClass $sc
      * @deprecated
      */
-    public function updateStripeData(\Stripe\Charge $sc)
+    public function updateStripeData(\Stripe\Charge $sc): void
     {
         $this->stripeId = $sc->id;
         $this->status = $sc->status;
@@ -99,12 +117,12 @@ class Charge
         $this->created = $sc->created;
     }
 
-    public function getId()
+    public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function isSucceeded()
+    public function isSucceeded(): bool
     {
         return $this->status === 'succeeded';
     }
@@ -112,7 +130,7 @@ class Charge
     /**
      * @return Question
      */
-    public function getQuestion()
+    public function getQuestion(): ?Question
     {
         return $this->question;
     }
@@ -120,7 +138,7 @@ class Charge
     /**
      * @return Customer
      */
-    public function getFromCustomer()
+    public function getFromCustomer(): ?Customer
     {
         return $this->fromCustomer;
     }
@@ -128,24 +146,24 @@ class Charge
     /**
      * @return Account|null
      */
-    public function getToAccount()
+    public function getToAccount(): ?Account
     {
         return $this->toAccount;
     }
 
     /**
-     * @return mixed
+     * @return int
      */
-    public function getAmount()
+    public function getAmount(): ?int
     {
         return $this->amount;
     }
 
     /**
-     * @param mixed $amount
+     * @param int $amount
      * @return Charge
      */
-    public function setAmount($amount)
+    public function setAmount(int $amount): Charge
     {
         $this->amount = $amount;
 
@@ -153,18 +171,18 @@ class Charge
     }
 
     /**
-     * @return mixed
+     * @return string
      */
-    public function getStripeId()
+    public function getStripeId(): ?string
     {
         return $this->stripeId;
     }
 
     /**
-     * @param mixed $stripeId
+     * @param string $stripeId
      * @return Charge
      */
-    public function setStripeId($stripeId)
+    public function setStripeId(string $stripeId): Charge
     {
         $this->stripeId = $stripeId;
 
@@ -172,18 +190,18 @@ class Charge
     }
 
     /**
-     * @return mixed
+     * @return string
      */
-    public function getStatus()
+    public function getStatus(): ?string
     {
         return $this->status;
     }
 
     /**
-     * @param mixed $status
+     * @param string $status
      * @return Charge
      */
-    public function setStatus($status)
+    public function setStatus(string $status): Charge
     {
         $this->status = $status;
 
@@ -191,18 +209,18 @@ class Charge
     }
 
     /**
-     * @return mixed
+     * @return int
      */
-    public function getApplicationFee()
+    public function getApplicationFee(): ?int
     {
         return $this->applicationFee;
     }
 
     /**
-     * @param mixed $applicationFee
+     * @param int $applicationFee
      * @return Charge
      */
-    public function setApplicationFee($applicationFee)
+    public function setApplicationFee(int $applicationFee): Charge
     {
         $this->applicationFee = $applicationFee;
 
@@ -210,18 +228,18 @@ class Charge
     }
 
     /**
-     * @return mixed
+     * @return string
      */
-    public function getCurrency()
+    public function getCurrency(): ?string
     {
         return $this->currency;
     }
 
     /**
-     * @param mixed $currency
+     * @param string $currency
      * @return Charge
      */
-    public function setCurrency($currency)
+    public function setCurrency(string $currency): Charge
     {
         $this->currency = $currency;
 
@@ -229,18 +247,18 @@ class Charge
     }
 
     /**
-     * @return mixed
+     * @return string
      */
-    public function getReceiptNumber()
+    public function getReceiptNumber(): ?string
     {
         return $this->receiptNumber;
     }
 
     /**
-     * @param mixed $receiptNumber
+     * @param string $receiptNumber
      * @return Charge
      */
-    public function setReceiptNumber($receiptNumber)
+    public function setReceiptNumber(string $receiptNumber): Charge
     {
         $this->receiptNumber = $receiptNumber;
 
@@ -248,25 +266,25 @@ class Charge
     }
 
     /**
-     * @return mixed
+     * @return int
      */
-    public function getCreated()
+    public function getCreated(): ?int
     {
         return $this->created;
     }
 
     /**
-     * @param mixed $created
+     * @param int $created
      * @return Charge
      */
-    public function setCreated($created)
+    public function setCreated(int $created): Charge
     {
         $this->created = $created;
 
         return $this;
     }
 
-    public function toArray()
+    public function toArray(): array
     {
         return [
             'receipt_number' => $this->receiptNumber,
