2a885d51155cd0bb9304c7bceb28fe0f23c361f9
Igor Gaponov <jiminy96@gmail.com>
2017-11-21 20:19:13 +0500

    Delete redirect from authentication entry point

--- backend/src/Civix/ApiBundle/Security/Http/EntryPoint/BearerTokenAuthenticationEntryPoint.php
+++ backend/src/Civix/ApiBundle/Security/Http/EntryPoint/BearerTokenAuthenticationEntryPoint.php
@@ -10,22 +10,11 @@ use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface
 
 class BearerTokenAuthenticationEntryPoint implements AuthenticationEntryPointInterface
 {
-    /**
-     * @var RouterInterface
-     */
-    private $router;
-
-    public function __construct(RouterInterface $router)
-    {
-        $this->router = $router;
-    }
-
     public function start(Request $request, AuthenticationException $authException = null)
     {
         return new JsonResponse(
             ['message' => 'Authentication failed.'],
-            JsonResponse::HTTP_UNAUTHORIZED,
-            ['Location' => $this->router->generate('api_secure_login')]
+            JsonResponse::HTTP_UNAUTHORIZED
         );
     }
 }
